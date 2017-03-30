9df1ea86c05b2049932234c5cfc47aebaa78a63d
pradosavljevic <pradosavljevic@student.tgm.ac.at>
2017-03-30 13:37:05 +0200

    Commit 3

--- src/radosavljevic/Test0330.java
+++ src/radosavljevic/Test0330.java
@@ -4,7 +4,7 @@ public class Test0330 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Guten Tag Hr. Radosavljevic");
+		System.out.println("Good morning Hr. Radosavljevic");
 
 	}
 
