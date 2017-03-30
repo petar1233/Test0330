109fda129b90a8782fecd0e60cb58d5bacc2b4c3
pradosavljevic <pradosavljevic@student.tgm.ac.at>
2017-03-30 13:35:03 +0200

    Commit 1

---
+++ src/radosavljevic/Test0330.java
@@ -0,0 +1,11 @@
+package radosavljevic;
+
+public class Test0330 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Guten Tag Hr. Radosavljevic");
+
+	}
+
+}
